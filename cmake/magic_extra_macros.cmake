c2630f29574d41fac414e75638856c7fdb282359
Rahul Mahajan <aerorahul@users.noreply.github.com>
2019-11-21 16:16:32 -0500

    add geometry.yaml file and macros
    Change-Id: I5d30e47a390999542169e3260e3034bf73e462cd

---
+++ cmake/magic_extra_macros.cmake
@@ -0,0 +1,11 @@
+# Extra macros to eliminate repetition
+
+# Macro to SYMLINK a FILELIST from SRCDIR to DSTDIR
+macro( SYMLINK_FILES FILELIST SRCDIR DSTDIR )
+  foreach( FILENAME ${FILELIST} )
+    execute_process( COMMAND ${CMAKE_COMMAND} -E create_symlink
+      ${SRCDIR}/${FILENAME}
+      ${DSTDIR}/${FILENAME} )
+  endforeach( FILENAME )
+endmacro()
+
