48b696dea116036bc151c7f46ad936c8172b776d
Rahul Mahajan <aerorahul@users.noreply.github.com>
2024-05-21 10:34:50 -0400

    remove copyright statements from files

--- test/executables/TestGeometry.cc
+++ test/executables/TestGeometry.cc
@@ -1,10 +1,3 @@
-/*
- * (C) Copyright 2019-2021 NOAA/NWS/NCEP/EMC.
- *
- * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 #include "magic/Traits.h"
 #include "oops/runs/Run.h"
 #include "test/interface/Geometry.h"
@@ -14,4 +7,3 @@ int main(int argc,  char ** argv) {
   test::Geometry<magic::Traits> tests;
   return run.execute(tests);
 }
-
